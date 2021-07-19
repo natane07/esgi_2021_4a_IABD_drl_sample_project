2d7793b6517ab7ecad77dc022753dfc34f4c2fc1
Natane <natane-b@hotmail.fr>
2021-07-19 11:57:04 +0200

    Algo temporal different architecture file

---
+++ drl_sample_project_python/drl_lib/algo_rl/q_learning.py
@@ -0,0 +1,14 @@
+import numpy as np
+from tqdm import *
+from ..do_not_touch.contracts import SingleAgentEnv
+from ..do_not_touch.result_structures import PolicyAndActionValueFunction
+
+
+
+def q_learning(
+        env: SingleAgentEnv,
+        alpha: float,
+        epsilon: float,
+        gamma: float,
+        max_iter: int) -> PolicyAndActionValueFunction:
+    pass
